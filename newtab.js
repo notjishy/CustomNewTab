a99ebc9b198d71448bd1c3116047b968004d0602
NotJishy <jishydoods@gmail.com>
2021-04-23 22:49:02 -0400

    idk how to describe what i did but there are now error handling things and stuff for missing wallpapers its cool ok
    Version 0.2

--- newtab.js
+++ newtab.js
@@ -1,10 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {    
-    function addImagesToOverlayWrapperMenu(image) {
+    function addImagesToOverlayWrapperMenu(image, amount) {
         var wrapper = document.getElementById("overlay-wrapper")
+
+        let id = amount
+        var divElement = document.createElement("div")
+        divElement.setAttribute("ID", `imgButtonDiv-${id}`)
+        divElement.setAttribute("class", "imgButtonDiv")
+
+        wrapper.appendChild(divElement)
+
         var element = document.createElement("img")
         element.src = image
-        element.alt = `${image}`
+        element.alt = `${element.src}`
         element.class = "wallpaperButton"
+
+        element.onerror = function() {
+            console.log(`Wallpaper ${id} could not be found`)
+            element.src = "./assets/wallpapers/fallback.png"
+            element.alt= `${element.src}`
+
+            divElement.appendChild(element)
+            divElement.style.width = `${element.width}px`
+            console.log(`${id} - ${element.width}px`)
+
+            var x = document.createElement("span")
+            x.setAttribute("ID", "imgLoadErrText")
+            x.setAttribute("class", "material-icons")
+            x.innerText = "report_gmailerrorred"
+
+            divElement.appendChild(x)
+
+            var alert = document.getElementById("errorAlert")
+            alert.style.display = "block"
+            alert.style.opacity = "100"
+        }
+
+        element.onload = function() {
+            divElement.appendChild(element)
+            divElement.style.width = `${element.width}px`
+            console.log(`${id} - ${element.width}px`)
+        }
 
         element.addEventListener('click', function() {
             document.body.style.backgroundImage = `url(${this.alt})`
@@ -13,9 +48,48 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.log(`Wallpaper changed`)
             })
         })
-
-        wrapper.appendChild(element)
     }
+
+    // Open settings overlay
+    document.getElementById("settings").addEventListener('click', function() {
+        var wrapper = document.getElementById("overlay-wrapper")
+
+        document.getElementById("overlay").style.height = "100%"
+        wrapper.style.display = "block"
+        document.getElementById("overlay-wrapper-2").style.display = "block"
+
+        var element = document.createElement("img")
+        element.src = "assets/wallpapers/add.png"
+        element.id = "addNew"
+        element.className = "addNew"
+        element.addEventListener('click', function() {
+            let menu = document.getElementById("addNewMenu")
+
+            document.getElementById("container").style.visibility = "hidden"
+
+            menu.style.display = "inline"
+
+            document.getElementById("overlay").style.height = "0";
+            document.getElementById("overlay-wrapper").style.display = "none"
+            document.getElementById("overlay-wrapper-2").style.display = "none"
+
+            removeImagesFromOverlayWrapperMenu()
+        })
+        wrapper.appendChild(element)
+
+        chrome.storage.local.get({userWallpapers: []}, function(result) {
+            var wallpapers = result.userWallpapers
+
+            var amount = 0
+            wallpapers.forEach(handleWallpaperImages)
+
+            function handleWallpaperImages(image) {
+                amount++
+
+                addImagesToOverlayWrapperMenu(image, amount)
+            }
+        })
+    })
 
     function removeImagesFromOverlayWrapperMenu() {
         var wrapper = document.getElementById("overlay-wrapper")
@@ -92,40 +166,6 @@ document.addEventListener('DOMContentLoaded', function() {
             setTimeout(clock, 500)
         }
     }
-
-    // Open settings overlay
-    document.getElementById("settings").addEventListener('click', function() {
-        var wrapper = document.getElementById("overlay-wrapper")
-
-        document.getElementById("overlay").style.height = "100%"
-        wrapper.style.display = "block"
-        document.getElementById("overlay-wrapper-2").style.display = "block"
-
-        var element = document.createElement("img")
-        element.src = "assets/wallpapers/add.png"
-        element.id = "addNew"
-        element.className = "addNew"
-        element.addEventListener('click', function() {
-            let menu = document.getElementById("addNewMenu")
-
-            document.getElementById("container").style.visibility = "hidden"
-    
-            menu.style.display = "inline"
-
-            document.getElementById("overlay").style.height = "0";
-            document.getElementById("overlay-wrapper").style.display = "none"
-            document.getElementById("overlay-wrapper-2").style.display = "none"
-
-            removeImagesFromOverlayWrapperMenu()
-        })
-        wrapper.appendChild(element)
-
-        chrome.storage.local.get({userWallpapers: []}, function(result) {
-            var wallpapers = result.userWallpapers
-
-            wallpapers.forEach(addImagesToOverlayWrapperMenu)
-        })
-    })
 
     // Close settings overlay
     document.getElementById("close-overlay").addEventListener('click', function() {
@@ -284,12 +324,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Close popup changes alert
     document.getElementById("alert-close").addEventListener('click', function() {
-        const alert = document.getElementById("changesAlert")
+        const alert = document.getElementById("errorAlert")
 
         alert.style.opacity = "0"
 
         setTimeout(function() {
-            document.getElementById("changesAlert").style.display = "none"
+            document.getElementById("errorAlert").style.display = "none"
         }, 1000)
     })
 })
